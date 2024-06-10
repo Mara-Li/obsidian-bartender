7ca6562dafbcc76cf4bbe116837ccf31332af7bf
Mara <mara-li@outlook.fr>
2024-06-10 08:40:12 +0200

    feat: add an option to disable the collapse for ribbon & status bar

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -1,5 +1,5 @@
-import { App, PluginSettingTab, Setting } from "obsidian";
-import BartenderPlugin from "../main";
+import { type App, PluginSettingTab, Setting } from "obsidian";
+import type BartenderPlugin from "../main";
 
 export interface BartenderSettings {
   statusBarOrder: string[];
@@ -9,7 +9,8 @@ export interface BartenderSettings {
   autoHide: boolean;
   autoHideDelay: number;
   dragDelay: number;
-  sortOrder:string;
+  sortOrder: string;
+  useCollapse: boolean;
 }
 
 export const DEFAULT_SETTINGS: BartenderSettings = {
@@ -20,7 +21,8 @@ export const DEFAULT_SETTINGS: BartenderSettings = {
   autoHide: false,
   autoHideDelay: 2000,
   dragDelay: 200,
-    sortOrder: "alphabetical"
+  sortOrder: "alphabetical",
+  useCollapse: true,
 };
 
 export class SettingTab extends PluginSettingTab {
@@ -31,18 +33,30 @@ export class SettingTab extends PluginSettingTab {
     this.plugin = plugin;
   }
 
-  hide() {}
-
   display(): void {
     const { containerEl } = this;
 
     containerEl.empty();
 
     new Setting(containerEl)
+      .setName("Support collapse")
+      .setDesc("Add a button to collapse the ribbon and status bar items")
+      .addToggle((toggle) =>
+        toggle.setValue(this.plugin.settings.useCollapse).onChange((value) => {
+          this.plugin.settings.useCollapse = value;
+          this.plugin.saveSettings();
+          this.display();
+        })
+      );
+    if (this.plugin.settings.useCollapse) { 
+
+    new Setting(containerEl)
       .setName("Auto Collapse")
-      .setDesc("Automatically hide ribbon and status bar items once your mouse leaves the icon container")
-      .addToggle(toggle =>
-        toggle.setValue(this.plugin.settings.autoHide).onChange(value => {
+      .setDesc(
+        "Automatically hide ribbon and status bar items once your mouse leaves the icon container"
+      )
+      .addToggle((toggle) =>
+        toggle.setValue(this.plugin.settings.autoHide).onChange((value) => {
           this.plugin.settings.autoHide = value;
           this.plugin.saveSettings();
         })
@@ -50,25 +64,30 @@ export class SettingTab extends PluginSettingTab {
 
     new Setting(containerEl)
       .setName("Auto Collapse Delay")
-      .setDesc("How long to wait before auto collapsing hidden icons on the ribbon and status bar")
-      .addText(textfield => {
+      .setDesc(
+        "How long to wait before auto collapsing hidden icons on the ribbon and status bar"
+      )
+      .addText((textfield) => {
         textfield.setPlaceholder(String(2000));
         textfield.inputEl.type = "number";
         textfield.setValue(String(this.plugin.settings.autoHideDelay));
-        textfield.onChange(async value => {
+        textfield.onChange(async (value) => {
           this.plugin.settings.autoHideDelay = Number(value);
           this.plugin.saveSettings();
         });
       });
+    }
 
     new Setting(containerEl)
       .setName("Drag Start Delay (ms)")
-      .setDesc("How long to wait before triggering the drag behavior after clicking. ⚠️ Requires an app restart.")
-      .addText(textfield => {
+      .setDesc(
+        "How long to wait before triggering the drag behavior after clicking. ⚠️ Requires an app restart."
+      )
+      .addText((textfield) => {
         textfield.setPlaceholder(String(200));
         textfield.inputEl.type = "number";
         textfield.setValue(String(this.plugin.settings.dragDelay));
-        textfield.onChange(async value => {
+        textfield.onChange(async (value) => {
           this.plugin.settings.dragDelay = Number(value);
           this.plugin.saveSettings();
         });
